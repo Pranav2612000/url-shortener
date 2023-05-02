9edc0a3119c8b211679a96f18e81bea046ef7421
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-05-02 06:46:28 +0530

    feat: create knockknock endpoint

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::{Build, Rocket};
+
+#[get("/knockknock")]
+fn knockknock() -> &'static str {
+    "Who's there?"
+}
+
+#[shuttle_service::main]
+async fn init() -> Result<Rocket<Build>, shuttle_service::Error> {
+    let rocket = rocket.build().mount("/", routes![knockknock]);
+
+    Ok(rocket);
+}
