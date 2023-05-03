b0d275da3a0d3bed43dff062c8104cb7209eb933
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-05-03 08:42:19 +0530

    feat: add migrations when the app starts'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
+use shuttle_runtime::CustomError;
+use sqlx::migrate::Migrator;
 use sqlx::{PgPool};
 
 #[get("/knockknock")]
@@ -8,8 +10,12 @@ fn knockknock() -> &'static str {
     "Who's there?"
 }
 
+static MIGRATOR: Migrator = sqlx::migrate!("./db");
+
 #[shuttle_runtime::main]
-async fn init(#[shuttle_shared_db::Postgres] _pool: PgPool) -> shuttle_rocket::ShuttleRocket {
+async fn init(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_rocket::ShuttleRocket {
+    MIGRATOR.run(&pool).await.map_err(CustomError::new)?;
+
     let rocket = rocket::build().mount("/", routes![knockknock]);
 
     Ok(rocket.into())
