cda54ed3694dfa722163b6d6cd3fc820006e04d3
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-05-03 08:19:43 +0530

    feat: add postgres support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate rocket;
 
+use sqlx::{PgPool};
+
 #[get("/knockknock")]
 fn knockknock() -> &'static str {
     "Who's there?"
 }
 
 #[shuttle_runtime::main]
-async fn init() -> shuttle_rocket::ShuttleRocket {
+async fn init(#[shuttle_shared_db::Postgres] _pool: PgPool) -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build().mount("/", routes![knockknock]);
 
     Ok(rocket.into())
